460f35fcb359b2cb23398a36d7103388f7eba21b
Prathik Mishra <126965610+Prathik-mishra@users.noreply.github.com>
2025-03-19 16:17:35 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,21 @@
+import React from 'react';
 
-import React from "react";
-import './../styles/App.css';
+import Counter from './componets/counter';
+import './App.css';
 
-const App = () => {
+
+function App() {
   return (
-    <div>
-        {/* Do not remove the main div */}
+
+    <div className="App">
+      <header className="App-header">
+      <h1>Redux Counter App</h1>
+      <Counter />
+      </header>
     </div>
-  )
+    
+  );
 }
 
-export default App
+
+export default App;
